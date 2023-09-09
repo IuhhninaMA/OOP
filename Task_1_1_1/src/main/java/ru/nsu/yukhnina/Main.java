4efe6a9db8a158c6ea1443f80ea131e1a05fcf2c
solnyshko3622 <m.yukhnina@g.nsu.ru>
2023-09-10 01:55:40 +0700

    Good codestyle?.0

--- Task_1_1_1/src/main/java/ru/nsu/yukhnina/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/yukhnina/Main.java
@@ -2,13 +2,17 @@ package ru.nsu.yukhnina;
 
 /**Содержит основную функцию sort и вспомогательную heeapify.*/
 
-public class Main{
+public class Main
+{
     /**Постройте max-heap из входных данных.*/
-    public int[] sort(int arr[], int n){
-        for (int i = n / 2 - 1; i >= 0; i--){
+    public int[] sort(int arr[], int n)
+    {
+        for (int i = n / 2 - 1; i >= 0; i--)
+        {
             heapify(arr, n, i);
         }
-        for (int i = n - 1; i >= 0; i--){
+        for (int i = n - 1; i >= 0; i--)
+        {
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
@@ -19,17 +23,21 @@ public class Main{
 
     /** Функция для преобразования в кучу.*/
 
-    void heapify(int arr[], int n, int i){
+    void heapify(int arr[], int n, int i)
+    {
         int largest = i;
         int l = 2 * i + 1;
         int r = 2 * i + 2;
-        if (l < n && arr[l] > arr[largest]) {
+        if (l < n && arr[l] > arr[largest])
+        {
             largest = l;
         }
-        if (r < n && arr[r] > arr[largest]){
+        if (r < n && arr[r] > arr[largest])
+        {
             largest = r;
         }
-        if (largest != i){
+        if (largest != i)
+        {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;

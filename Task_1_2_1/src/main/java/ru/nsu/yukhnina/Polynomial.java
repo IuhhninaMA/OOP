1073c6fbdcc43e0f8f35fea7817c06d766756ad1
Marya Yukhnina <maryayukhnina@MacBook-Air-Marya.local>
2023-09-21 19:36:51 +0300

    first normal attempt

--- Task_1_2_1/src/main/java/ru/nsu/yukhnina/Polynomial.java
+++ Task_1_2_1/src/main/java/ru/nsu/yukhnina/Polynomial.java
@@ -1,62 +1,179 @@
 package ru.nsu.yukhnina;
 
-import java.util.Arrays;
+import static java.lang.String.valueOf;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+/**
+* Класс, в котором определены все штучки для многочлена.
+* Сложениеб вычитание, умножение, нахождение производных любого порядка.
+*/
 public class Polynomial {
-    float[] indexes;
-    int len_indexes_array;
-    Polynomial(float[] array){
-        indexes = array;
-        len_indexes_array = array.length;
+    float[] polynomIndexes;
+    int maxIndex;
+    Polynomial(float[] array) {
+        polynomIndexes = array;
+        maxIndex = array.length;
     }
 
-    Polynomial addition(Polynomial p){
-
-        int minLenght = Math.min(len_indexes_array, p.len_indexes_array);
-        int maxLenght = Math.max(len_indexes_array, p.len_indexes_array);
+    /**
+    * Функция для сложения двух многочленов
+    * @param p - второе слагаемое
+    * @return результат сложения двух многочленов.
+    */
+    Polynomial addition(Polynomial p) {
+        int minLenght = Math.min(maxIndex, p.maxIndex);
+        int maxLenght = Math.max(maxIndex, p.maxIndex);
         Polynomial result = new Polynomial(new float[maxLenght]);
-
         //перебираем всё до конца наименьшего массива
-        for (int i = 0; i < minLenght; i++){
-            result.indexes[i] = indexes[i] + p.indexes[i];
+        for (int i = 0; i < minLenght; i++) {
+            result.polynomIndexes[i] = polynomIndexes[i] + p.polynomIndexes[i];
         }
-
         //если массивы не равны по длинне, то должно ещё что-то остаться, добавляем
-        for (int i = 0; i < maxLenght - p.len_indexes_array; i++){
-            result.indexes[minLenght+i] = indexes[minLenght+i];
-        }
-        for (int i = 0; i < maxLenght - len_indexes_array; i++){
-            result.indexes[minLenght+i] = p.indexes[minLenght+i];
-        }
+        if (maxLenght - p.maxIndex >= 0)
+            System.arraycopy(polynomIndexes, minLenght + 0, result.polynomIndexes, minLenght + 0, maxLenght - p.maxIndex);
+        if (maxLenght - maxIndex >= 0)
+            System.arraycopy(p.polynomIndexes, minLenght + 0, result.polynomIndexes, minLenght + 0, maxLenght - maxIndex);
         return result;
     }
 
 
-    Polynomial difference(Polynomial p){
-        int minLenght = Math.min(len_indexes_array, p.len_indexes_array);
-        int maxLenght = Math.max(len_indexes_array, p.len_indexes_array);
+    /**
+    * Для нахождения разности между многочленами
+    * @param p - вычитаемое
+    * @return разность многочленов.
+    */
+    Polynomial difference(Polynomial p) {
+        int minLenght = Math.min(maxIndex, p.maxIndex);
+        int maxLenght = Math.max(maxIndex, p.maxIndex);
         Polynomial result = new Polynomial(new float[maxLenght]);
         //перебираем всё до конца наименьшего массива
-        for (int i = 0; i < minLenght; i++){
-            result.indexes[i] = indexes[i] - p.indexes[i];
+        for (int i = 0; i < minLenght; i++) {
+            result.polynomIndexes[i] = polynomIndexes[i] - p.polynomIndexes[i];
         }
         //если массивы не равны по длинне, то должно ещё что-то остаться, добавляем
-        for (int i = 0; i < maxLenght - p.len_indexes_array; i++){
-            result.indexes[minLenght+i] = indexes[minLenght+i];
+        if (maxLenght - p.maxIndex >= 0)
+            System.arraycopy(polynomIndexes, minLenght + 0, result.polynomIndexes, minLenght + 0, maxLenght - p.maxIndex);
+        for (int i = 0; i < maxLenght - maxIndex; i++) {
+            result.polynomIndexes[minLenght+i] = -p.polynomIndexes[minLenght+i];
         }
-
-        for (int i = 0; i < maxLenght - len_indexes_array; i++){
-            result.indexes[minLenght+i] = -p.indexes[minLenght+i];
-        }
-
         return result;
     }
 
-    public static void main(String[] args){
+    /**
+    * @param p - второй множитель
+    * @return произведение многочленов.
+    */
+    Polynomial mult(Polynomial p) {
+        int maxLenght = Math.max(maxIndex, p.maxIndex);
+        float[] result_array = new float[maxLenght*2] ;
+        Polynomial result = new Polynomial(result_array);
+        for (int i = 0; i < maxIndex; i++) {
+            for (int j = 0; j < p.maxIndex; j++){
+                result.polynomIndexes[i+j] += polynomIndexes[i]*p.polynomIndexes[j];
+            }
+        }
+        return result;
+    }
+
+    /**
+    * @param x - точка,в которой нужно вычислить значение многочлена
+    * @return чему равен многочлен в точке x
+    */
+    float calculationAtPoint(float x) {
+        float helperX = 1, result = 0;
+        for (int i = 0; i < maxIndex; i++){
+            result += helperX* polynomIndexes[i];
+            helperX *= x;
+        }
+        return result;
+    }
+
+    /**
+    * @param n - порядок производной
+    * @return производная n-ной степени многочлена.
+    */
+    Polynomial differential(int n) {
+        Polynomial result = new Polynomial(polynomIndexes);
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < result.maxIndex; j++) {
+                result.polynomIndexes[j - 1] = result.polynomIndexes[j] * j;
+            }
+            result.maxIndex--;
+            result.polynomIndexes[result.maxIndex] = 0;
+
+        }
+        return result;
+    }
+
+    /**
+    * @param p - многочлен, с которым нужно сравнить
+    * @return true or fale  или равны многочлены или нет.
+    */
+    boolean compare(Polynomial p) {
+        if (maxIndex != p.maxIndex) {
+            return false;
+        }
+        for (int i = 0; i < maxIndex; i++) {
+            if (polynomIndexes[i] != p.polynomIndexes[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** переводит полином из вида, удобного программе в человеческий вид
+    * @return многочлен в человеческом виде.
+    */
+    String printToString() {
+        String result = new String();
+        boolean sign = false;//false  если ещё не выводились + или -
+        if (polynomIndexes[0] != 0) {
+            if (polynomIndexes[0] < 0){
+                result = result.concat("- ");
+            }
+            result = result.concat(valueOf(Math.abs(polynomIndexes[0])));
+            sign = true;
+        }
+        //первый случай я рассматриваю отдельно тк не x^k, а просто x
+        if (maxIndex > 0) {
+            if (polynomIndexes[1] < 0){
+                result = result.concat(" - ");
+            } else if (sign) {
+                result = result.concat(" + ");
+            }
+            result = result.concat(valueOf(Math.abs(polynomIndexes[1]) + "x"));
+            sign = true;
+            }
+
+        for (int i = 2; i < maxIndex; i++){
+            if (polynomIndexes[i] < 0){
+                result = result.concat(" - ");
+                sign = true;
+            } else if (sign && polynomIndexes[i] != 0) {
+                result = result.concat(" + ");
+            }
+            if (polynomIndexes[i] != 0){
+                result = result.concat(valueOf(Math.abs(polynomIndexes[i]) + "x^" + i));
+                sign = true;
+            }
+
+
+        }
+        return result;
+    }
+
+    /** Ну main и main
+     * @param args
+     */
+    public static void main(String[] args) {
         Polynomial p1 = new Polynomial(new float[] {4, 3, 6, 7});
         Polynomial p2 = new Polynomial(new float[] {3, 2, 8});
-        System.out.println(Arrays.toString(p1.difference(p2).indexes));
+        System.out.println(p1.difference(p2).printToString());
+        Polynomial p3 = new Polynomial(new float[] {1, 0, 4, 5, 0, 0, 0, 1});
+        Polynomial p4 = new Polynomial(new float[] {0, 3, 1});
+        System.out.println(p3.mult(p4).printToString());
+        float res = p3.calculationAtPoint(2);
+        float res2 = p1.calculationAtPoint(2);
+        System.out.println(p3.differential(2).printToString());
     }
 }
---
+++ Task_1_2_1/src/test/java/ru/nsu/yukhnina/PolynomialTest.java
@@ -0,0 +1,81 @@
+package ru.nsu.yukhnina;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolynomialTest {
+
+    @Test
+    void addWithDifLen() {
+        Polynomial p1 = new Polynomial(new float[] {4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new float[] {3, 2, 8});
+        assertEquals(p1.addition(p2).printToString(), "7.0 + 5.0x + 14.0x^2 + 7.0x^3");
+    }
+
+    @Test
+    void addFloatWithEqLen() {
+        Polynomial p3 = new Polynomial(new float[] {4, 3.2F, 6, 7});
+        Polynomial p4 = new Polynomial(new float[] {3, 2, 8, 10.5F});
+        assertEquals(p3.addition(p4).printToString(), "7.0 + 5.2x + 14.0x^2 + 17.5x^3");
+    }
+
+    @Test
+    void differenceWithEqLen() {
+        Polynomial p3 = new Polynomial(new float[] {4, 3.2F, 6, 7});
+        Polynomial p4 = new Polynomial(new float[] {3, 2, 8, 10.5F});
+        assertEquals(p3.difference(p4).printToString(), "1.0 + 1.2x - 2.0x^2 - 3.5x^3");
+    }
+
+    @Test
+    void differenceWithDifLen() {
+        Polynomial p5 = new Polynomial(new float[] {4, 3.2F, 6, 7});
+        Polynomial p6 = new Polynomial(new float[] {3, 2, 8});
+        assertEquals(p5.difference(p6).printToString(), "1.0 + 1.2x - 2.0x^2 + 7.0x^3");
+    }
+
+    @Test
+    void differenceWithDifLen2() {
+        Polynomial p5 = new Polynomial(new float[] {4, 3.2F, 6});
+        Polynomial p6 = new Polynomial(new float[] {3, 2, 8, 10});
+        assertEquals(p5.difference(p6).printToString(), "1.0 + 1.2x - 2.0x^2 - 10.0x^3");
+    }
+
+    @Test
+    void mult() {
+        Polynomial p7 = new Polynomial(new float[] {1, 0, 4, 5, 0, 0, 0, 1});
+        Polynomial p8 = new Polynomial(new float[] {0, 3, 1});
+        assertEquals(p7.mult(p8).printToString(), "3.0x + 1.0x^2 + 12.0x^3 + 19.0x^4 + 5.0x^5 + 3.0x^8 + 1.0x^9");
+    }
+
+    @Test
+    void calculationAtPoint1() {
+        Polynomial p9 = new Polynomial(new float[] {112, 2, 3, 8, 9, 10, 12});
+        assertEquals(p9.calculationAtPoint(1), 156);
+    }
+
+    @Test
+    void calculationAtPoint2() {
+        Polynomial p10 = new Polynomial(new float[] {112, 2, 3, 8, 9, 10, 12});
+        assertEquals(p10.calculationAtPoint(2), 1424.0);
+    }
+
+    @Test
+    void differential() {
+        Polynomial p11 = new Polynomial(new float[] {112, 2, 3, 8, 9, 10, 12});
+        assertArrayEquals(p11.differential(2).polynomIndexes, new float[] {6, 48, 108, 200, 360, 0, 0});
+    }
+
+    @Test
+    void compare() {
+        Polynomial p12 = new Polynomial(new float[] {1, 0, 4, 5, 0, 0, 0, 1});
+        Polynomial p13 = new Polynomial(new float[] {0, 3, 1});
+        assertEquals(p12.compare(p13), false);
+    }
+
+    @Test
+    void compare2() {
+        Polynomial p14 = new Polynomial(new float[] {});
+        assertEquals(p14.compare(p14), true);
+    }
+}

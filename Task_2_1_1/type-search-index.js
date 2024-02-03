686a74f8da59d73afb7efecd8df9ada077b26cd9
solnyshko3622 <solnyshko3622@users.noreply.github.com>
2024-02-03 16:10:25 +0000

    Deploying to gh-pages from @ solnyshko3622/OOP@7475951878d22d6e7aac0d01a3b0e859aeed1ca4 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.yukhnina","l":"OneStream"},{"p":"ru.nsu.yukhnina","l":"PrimeNumParallel"},{"p":"ru.nsu.yukhnina","l":"PrimeNumSequent"},{"p":"ru.nsu.yukhnina","l":"PrimeNuSterams"}]

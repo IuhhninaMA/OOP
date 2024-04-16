a6cff6f588992a97f659286ef9e43f2afedb365f
Marya Yukhnina <m.yukhnina@g.nsu.ru>
2024-04-16 11:07:36 +0700

    Fix test

--- Task_2_2_1/src/test/java/ru/nsu/yukhnina/BakerTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/yukhnina/BakerTest.java
@@ -7,12 +7,12 @@ class BakerTest {
     void cook() throws InterruptedException {
         TaskQueue tasks = new TaskQueue();
         TaskQueue tasksC = new TaskQueue(100L);
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 50; i++) {
             tasks.addTask(new Task("Pizza", "Address", 1, i));
         }
         Baker baker = new Baker(1, tasks, tasksC, "Barbie");
         Thread.sleep(400);
         baker.interrupt();
-        assert (baker.howPizzas() > 0);
+        assert (baker.howPizzas() > 25);
     }
 }
